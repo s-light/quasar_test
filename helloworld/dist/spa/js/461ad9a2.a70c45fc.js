e024c6375ce764fda8bbb4251ba461c2566f036a
s-light <github@s-light.eu>
2019-05-31 00:56:34 +0200

    extended font example - added filter function - added font Titillium - added font RujisHandwritingFontv20 - added font Office Code Pro and build.

---
+++ helloworld/dist/spa/js/461ad9a2.a70c45fc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["461ad9a2"],{"9fc0":function(e,t,a){"use strict";a.r(t);var s={name:"PageIndex"},l=a("2877"),n=Object(l.a)(s,function(){var e=this.$createElement,t=this._self._c||e;return t("q-page",{staticClass:"flex flex-center"},[t("img",{attrs:{alt:"Quasar logo",src:a("d3d6")}}),t("h1",[this._v("!?Help?!")])])},[],!1,null,null,null);t.default=n.exports},d3d6:function(e,t,a){e.exports=a.p+"img/quasar-logo-full.c3e88651.svg"}}]);
+//# sourceMappingURL=461ad9a2.a70c45fc.js.map
--- helloworld/src/pages/font.vue
+++ helloworld/src/pages/font.vue
@@ -1,44 +1,60 @@
 <template>
     <q-page class="flex flex-center">
         <section>
+            <div
+                :style="{fontFamily : font_active}"
+            >
+                {{ testmessage }}
+            </div>
+
+            <br>
+            <br>
+
+            <q-input
+                clearable
+                filled
+                v-model="testmessage"
+                label="Test Message"
+            />
+
             <q-select
                 filled
-                v-model="font_active"
-                :options="font_list"
                 label="select font"
-            />
-            <!-- https://stackoverflow.com/a/52599854/574981 -->
-            <select v-model="font_active">
-                <option
-                    value=""
-                    disabled
-                    selected
-                >
-                    Font
-                </option>
-                <option
-                    v-for="(font, index) in font_list"
-                    :key="index"
-                    :value="font"
-                    :style="{fontFamily : font}"
-                >
-                    {{ font }}
-                </option>
-            </select>
-            <div>
-                <q-input
-                    clearable
-                    filled
-                    color="purple-12"
-                    v-model="testmessage"
-                    label="Test Message"
-                />
-                <div
-                    :style="{fontFamily : font_active}"
-                >
-                    {{ testmessage }}
-                </div>
-            </div>
+                hint="you can search.."
+                v-model="font_active"
+                :options="font_list_filtered"
+                use-input
+                input-debounce="0"
+                @filter="filterFn"
+            >
+                <template v-slot:no-option>
+                    <q-item>
+                        <q-item-section class="text-grey">
+                            No results
+                        </q-item-section>
+                    </q-item>
+                </template>
+                <template v-slot:option="scope">
+                    <q-item
+                        v-bind="scope.itemProps"
+                        v-on="scope.itemEvents"
+                    >
+                        <q-item-section>
+                            <q-item-label
+                                v-html="scope.opt"
+                                :style="{fontFamily : scope.opt}"
+                            />
+                        </q-item-section>
+                    </q-item>
+                </template>
+            </q-select>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            .
         </section>
     </q-page>
 </template>
@@ -51,14 +67,30 @@ export default {
     name: 'HidTest',
     data () {
         return {
-            testmessage: 'Here we have some text. This is only for testing the font...',
-            font_active: null,
+            testmessage: '16:05 Here we have some text. This is only for testing the font...',
+            font_active: 'Titillium',
             font_list: [
+                'Titillium',
+                'RujisHandwritingFontv20',
+                'Office Code Pro',
+                'Office Code Pro D',
+                'Roboto',
+                '-- Not a Font --',
                 'Overlock',
                 'Ruthie',
                 'Almendra',
                 'DejaVu Sans Condensed'
-            ]
+            ],
+            font_list_filtered: this.font_list
+        }
+    },
+    methods: {
+        // https://quasar.dev/vue-components/select#Example--Basic-autocomplete
+        filterFn (val, update, abort) {
+            update(() => {
+                const needle = val.toLowerCase()
+                this.font_list_filtered = this.font_list.filter(v => v.toLowerCase().indexOf(needle) > -1)
+            })
         }
     }
 }
