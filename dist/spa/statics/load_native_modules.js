008ffcef651c0ae09eb3beedec9ada8ed22ea095
s-light <git@s-light.eu>
2019-06-16 21:55:16 +0200

    [SPA] build

--- dist/spa/statics/load_native_modules.js
+++ dist/spa/statics/load_native_modules.js
@@ -1,13 +1,29 @@
+// eslint-disable-next-line no-unused-vars
 let SerialPort
+// eslint-disable-next-line no-unused-vars
+let HID
+
 try {
     if (process.versions.electron) {
         // eslint-disable-next-line no-console
-        console.info('Running electron → importing SerialPort')
+        console.info('Running electron.')
+
+        // eslint-disable-next-line no-console
+        console.info('→ importing SerialPort')
         try {
             SerialPort = require('serialport')
         } catch (e) {
             // eslint-disable-next-line no-console
             console.error('importing SerialPort failed:\n', e)
+        }
+
+        // eslint-disable-next-line no-console
+        console.info('→ importing HID')
+        try {
+            HID = require('node-hid')
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error('importing HID failed:\n', e)
         }
     }
 } catch (e) {
@@ -15,28 +31,28 @@ try {
     console.error('load_native_modules failed:\n', e)
 }
 
-if (SerialPort) {
-    try {
-        SerialPort.list().then(
-            ports => {
-                // eslint-disable-next-line no-console
-                console.group('SerialPort list:')
-                for (let port of ports) {
-                    if (port.vendorId) {
-                        // eslint-disable-next-line no-console
-                        console.log(port)
-                    }
-                }
-                // eslint-disable-next-line no-console
-                console.groupEnd()
-            },
-            err => {
-                // eslint-disable-next-line no-console
-                console.error('SerialPort.list:\n', err)
-            }
-        )
-    } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error('SerialPort.list()\n', e)
-    }
-}
+// if (SerialPort) {
+//     try {
+//         SerialPort.list().then(
+//             ports => {
+//                 // eslint-disable-next-line no-console
+//                 console.group('SerialPort list:')
+//                 for (let port of ports) {
+//                     if (port.vendorId) {
+//                         // eslint-disable-next-line no-console
+//                         console.log(port)
+//                     }
+//                 }
+//                 // eslint-disable-next-line no-console
+//                 console.groupEnd()
+//             },
+//             err => {
+//                 // eslint-disable-next-line no-console
+//                 console.error('SerialPort.list:\n', err)
+//             }
+//         )
+//     } catch (e) {
+//         // eslint-disable-next-line no-console
+//         console.error('SerialPort.list()\n', e)
+//     }
+// }
